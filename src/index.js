98205934c0d3ac18f6a70d5d990f3cc843e2ee0d
vu.nguyenhoang <rockmanx000@gmail.com>
2023-12-17 12:29:38 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
+const bodyParser = require('body-parser');
+const Multer = require('multer');
 
 const router = express.Router();
+const getFields = Multer();
+router.get("/",(req,res,next)=>{
+    console.log(path.resolve(path.join(__dirname,"..",'public','index.html')));
+    res.sendFile('index.html',{root:path.resolve(path.join(__dirname,"..",'public',))});
+});
 
-router.get("/login",(req,res,next)=>{
-    res.send("hello world");
-    next();
+router.post("/login",getFields.none(),(req,res)=>{
+    console.log(req.headers);
+    console.log(req.body);
+    res.status(200);
+    res.end();
+    
 });
 
 app.use("/",router);
